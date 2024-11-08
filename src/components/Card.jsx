b6e0dbcc394004427a8a10b2f19c424ab0a8cd19
Esteban Alban <esteban.alban9003@gmail.com>
2024-11-07 20:18:38 -0500

    Se agrega cambios a las cards por prioridad

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,17 @@
 import "./Card.css";
 
 const Card = ({ name, details, priority }) => {
+	const priorityClass =
+		priority.toLowerCase() === "alta"
+			? "high"
+			: priority.toLowerCase() === "media"
+			? "medium"
+			: priority.toLowerCase() === "baja"
+			? "low"
+			: "";
+
 	return (
-		<section className="card-container">
+		<section className={`card-container ${priorityClass}`}>
 			<h3>Tarea: {name}</h3>
 			<h4>Detalles: {details}</h4>
 			<h4>Prioridad: {priority}</h4>
